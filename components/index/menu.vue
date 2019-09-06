955a92cddc4806b97dd5bb5f6e5725965719a6a2
Liangxiyao <700680635@qq.com>
2019-09-06 18:39:57 +0800

    mt

---
+++ components/index/menu.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="m-menu">
+     <dl class="nav"  @mouseleave="mouseleave">
+       <dt>全部分类</dt>
+       <dd v-for="(item,idx) in menu" :key="idx" 
+            @mouseenter="enter">
+         <i :class="item.type"></i>{{item.name}}<span class="arrow"></span>
+       </dd>
+     </dl>
+     <div class="detail" v-if="kind" @mouseenter="hover" @mouseleave="out">
+       <template v-for="(item,idx) in curdetail.child">
+         <h4 :key="idx">{{item.title}}</h4>
+         <span v-for="v in item.child" :key="v">{{v}}</span>
+       </template>
+     </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        kind:'',  //hover时候的类型
+        menu: [
+          {
+            type:'food',
+            name:'美食',
+            child:[
+              {
+                title:'美食',
+                child:['饮品','代金券','小吃']
+              }
+            ]
+          },{
+            type:'takeout',
+            name:'外卖',
+            child:[
+              {
+                title:'外卖',
+                child:['美团外卖']
+              }
+            ]
+          },{
+            type:'hotel',
+            name:'酒店',
+            child:[
+              {
+                title:'酒店',
+                child:['经济型','舒适型','高档型']
+              }
+            ]
+          }
+        ]
+      }
+    },
+    computed: {
+      curdetail() {
+        return  this.menu.filter(item => item.type==this.kind)[0]
+      }
+    },
+    methods: {
+      mouseleave() {
+        this.timer = setTimeout(()=>{
+          this.kind = ''
+        },500)
+      },
+      enter(e){   
+        this.kind = e.target.querySelector('i').className
+      },
+      hover(){
+        clearTimeout(this.timer)
+      },
+      out(){
+        this.kind = ''
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
